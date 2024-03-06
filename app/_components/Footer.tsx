ecafa30750a51bed16a3cda7f8ca2adbbf750a5e
Abdullah-142 <abdulah14200@gmail.com>
2024-03-05 17:02:12 -0700

    commit no 3

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className=" bg-[#161921] flex justify-around py-8">
+    <footer className=" bg-[#161921] flex md:flex-row flex-col px-6 md:px-0 gap-5 items-center justify-around py-8">
       <div>
         <h1>@All copyright recive Animevalut</h1>
       </div>
